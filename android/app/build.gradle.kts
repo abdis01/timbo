7c051a2fd4896c7a7c0cfcff376ebde75ad014ad
timbo <abdimagoye26@gmail.com>
2026-06-09 14:04:40 +0300

    Fix Kotlin: let Flutter Gradle Plugin manage Kotlin version, apply kotlin plugin in app/build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -46,12 +47,6 @@ android {
         release {
             signingConfig = signingConfigs.getByName("release")
         }
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
